f2b926523508ad43e76eea83f662df9240039eb6
Yashwanth <140995223+Yash-git1235@users.noreply.github.com>
2024-04-18 23:44:45 +0530

    Update pid_gui.py

--- pid_gui.py
+++ pid_gui.py
@@ -1,7 +1,4 @@
 import time
-import sys
-import re
-import asyncio
 from tkinter import *
 
 # Import mavutil
@@ -24,9 +21,7 @@ def default(pname,value):
     mavutil.mavlink.MAV_PARAM_TYPE_UINT16  # Parameter type
     )
 
-ch3=-1
-ch3=int(input("Press 1 to load default pid values: "))
-if (ch3):
+def setdefault():
     default('ATC_RAT_RLL_P',0.135)
     default('ATC_RAT_RLL_I',0.135)
     default('ATC_RAT_RLL_D',0.0036)
@@ -36,6 +31,11 @@ if (ch3):
     default('ATC_RAT_YAW_P',0.180)
     default('ATC_RAT_YAW_I',0.018)
     default('ATC_RAT_YAW_D',0.000)
+
+ch3=-1
+ch3=int(input("Press 1 to load default pid values: "))
+if (ch3):
+    setdefault()
 else:
     pass
 
@@ -92,13 +92,16 @@ def uploadmission(aFileName):
     home_location = None
     home_altitude = None
     with open(aFileName) as f:
+        t=0
+        size=len(f.readlines())
+        f.seek(0)
         for i, line in enumerate(f):
             if i==0:
                 if not line.startswith('QGC WPL 110'):
                     raise Exception('File is not supported WP version')
             else:   
                 linearray=line.split('\t')
-                ln_seq = int(linearray[0])
+                ln_seq = int(linearray[0])+t
                 ln_current = int(linearray[1])
                 ln_frame = int(linearray[2])
                 ln_command = int(linearray[3])
@@ -110,13 +113,32 @@ def uploadmission(aFileName):
                 ln_y=float(linearray[9])
                 ln_z=float(linearray[10])
                 ln_autocontinue = int(linearray[11].strip())
-                if(i == 1):
+                if(i==1):
                     home_location = (ln_x,ln_y)
                     home_altitude = ln_z
-                p = mavutil.mavlink.MAVLink_mission_item_message(master.target_system, master.target_component, ln_seq, ln_frame,
-                                                                ln_command,
-                                                                ln_current, ln_autocontinue, ln_param1, ln_param2, ln_param3, ln_param4, ln_x, ln_y, ln_z)
-                wp.add(p)                                         
+                if(i==2 and ln_command!=22): #check for takeoff cmd in wp file
+                    t=1
+                elif(t==1 and i==3): #add takeoff cmd if not present
+                    p_tkf = mavutil.mavlink.MAVLink_mission_item_message(master.target_system, master.target_component, ln_seq-1, ln_frame, 22,
+                                                                    ln_current, ln_autocontinue, ln_param1, ln_param2, ln_param3, ln_param4, home_location[0], home_location[1], ln_z)
+                    wp.add(p_tkf)
+                    print("Takeoff command added.")
+                    p = mavutil.mavlink.MAVLink_mission_item_message(master.target_system, master.target_component, ln_seq, ln_frame,ln_command,
+                                                                    ln_current, ln_autocontinue, ln_param1, ln_param2, ln_param3, ln_param4, ln_x, ln_y, ln_z)
+                    wp.add(p)
+                elif(i==size-1 and ln_command!=20): #check and add RTL cmd if not present
+                    p = mavutil.mavlink.MAVLink_mission_item_message(master.target_system, master.target_component, ln_seq, ln_frame,ln_command,
+                                                                    ln_current, ln_autocontinue, ln_param1, ln_param2, ln_param3, ln_param4, ln_x, ln_y, ln_z)
+                    wp.add(p)
+                    ln_command=20
+                    p_rtl = mavutil.mavlink.MAVLink_mission_item_message(master.target_system, master.target_component, ln_seq+1, ln_frame, ln_command,
+                                                                    ln_current, ln_autocontinue, ln_param1, ln_param2, ln_param3, ln_param4, home_location[0], home_location[1], 0)
+                    wp.add(p_rtl)
+                    print("RTL command added.")
+                else: #add wp
+                    p = mavutil.mavlink.MAVLink_mission_item_message(master.target_system, master.target_component, ln_seq, ln_frame,ln_command,
+                                                                    ln_current, ln_autocontinue, ln_param1, ln_param2, ln_param3, ln_param4, ln_x, ln_y, ln_z)
+                    wp.add(p)                                         
     cmd_set_home(home_location,home_altitude)
     msg = master.recv_match(type = 'COMMAND_ACK',blocking = True)
     print(msg)
@@ -136,30 +158,19 @@ def uploadmission(aFileName):
         print('Sending waypoint {0}'.format(msg.seq))
 
 
-uploadmission('/home/yash/Downloads/random2.waypoints')
-
-time.sleep(1)
-#takeoff
-master.mav.command_long_send(
-    master.target_system,
-    master.target_component,
-    mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0,
-      13.0302279,
-      77.5665213,
-      10)
-msg = master.recv_match(type='COMMAND_ACK', blocking=True)
-print(msg)
+uploadmission('your_waypoint_file_here')
 
 def pread(param_name):
-    master = mavutil.mavlink_connection(connection_string)
+    #master = mavutil.mavlink_connection(connection_string)
     master.wait_heartbeat()
     master.param_fetch_one(param_name)
     param_value_msg = master.recv_match(type='PARAM_VALUE', blocking=True)
     param_value = param_value_msg.param_value
     return param_value
 
-time.sleep(1)
 #start automission
+time.sleep(1)
+master.wait_heartbeat()
 master.mav.command_long_send(master.target_system, master.target_component,
                                 300 , 0, 0, 0, 0, 0, 0, 0, 0)
 msg = master.recv_match(type='COMMAND_ACK', blocking=True)
@@ -190,7 +201,7 @@ def update():
 
 root = Tk()
 root.title('PID Tuning')
-root.geometry('500x200')
+root.geometry('500x250')
 
 frame1=Frame(root)
 frame1.pack()
@@ -200,6 +211,9 @@ frame2.pack()
 
 frame3=Frame(root)
 frame3.pack()
+
+frame4=Frame(root)
+frame4.pack(side='right')
 
 w = Label(frame1, text='Enter rate change percentage:')
 w.grid(row=0,column=0)
@@ -285,17 +299,11 @@ yawd.grid(row=2,column=9, padx=15)
 Button(frame3, text='+', command = lambda: button_func('ATC_RAT_YAW_D',entry_string,1)).grid(row=2,column=10)
 Button(frame3, text='-', command = lambda: button_func('ATC_RAT_YAW_D',entry_string,-1)).grid(row=2,column=11)
 
-exit=Button(frame3, text='EXIT', command = lambda: exitgui())
-exit.grid(row=3,column=5, padx=15)
+reset=Button(frame4, text='RELOAD DEFAULTS', command = lambda: setdefault())
+reset.grid(row=0, column=0, pady=20)
+
+exit=Button(frame4, text='EXIT', command = lambda: exitgui())
+exit.grid(row=0,column=1, pady=20, padx=40)
 
 update()
 root.mainloop()
-
-#rtl
-master.mav.command_long_send(
-master.target_system,
-master.target_component,
-20, 0, 0, 0, 0, 0, 0, 0, 0)
-msg = master.recv_match(type='COMMAND_ACK', blocking=True)
-print(msg)
-
